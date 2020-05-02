55c1036f67e0a387e92025db7a1180e24363f983
Rachel Eiting <reiting1@gmail.com>
2020-05-02 18:35:00 -0500

    finished part 1.7

--- Part 1/unicafe/unicafe/src/index.js
+++ Part 1/unicafe/unicafe/src/index.js
@@ -12,17 +12,21 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
 
   const handleGood = () => {
     setGood(good + 1);
+    setAll(all + 1);
   }
 
   const handleNeutral = () => {
     setNeutral(neutral + 1);
+    setAll(all + 1);
   }
 
   const handleBad = () => {
     setBad(bad + 1);
+    setAll(all + 1);
   }
 
   return (
@@ -39,6 +43,9 @@ const App = () => {
           <li>good: {good}</li>
           <li>neutral: {neutral}</li>
           <li>bad: {bad}</li>
+          <li>all: {all}</li>
+          <li>average: {(good * 1 + neutral * 0 + bad * -1) / all}</li>
+          <li>positive: {`${(good / all) * 100}%`}</li>
         </ul>
       </div>
     </div>
