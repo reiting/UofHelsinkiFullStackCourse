efaa12e42788392babe0209ec5769eb6ea89fc76
Rachel Eiting <reiting1@gmail.com>
2020-05-14 14:17:07 -0500

    Completed section 2.8 adding numbers to phonebook

--- Part 2/phonebook/src/App.js
+++ Part 2/phonebook/src/App.js
@@ -3,18 +3,24 @@ import './App.css';
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '205-364-8545' }
   ])
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
   }
 
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
+  }
+
   const addPerson = (event) => {
     event.preventDefault()
     const personObject = {
-      name: newName
+      name: newName,
+      number: newNumber
     }
     if (persons.some(cred => cred.name === newName)) {
       alert(`${newName} is already added to phonebook`);
@@ -32,13 +38,17 @@ const App = () => {
             value={newName}
             onChange={handleNameChange} />
         </div>
+        <div>number: <input 
+            value={newNumber} 
+            onChange={handleNumberChange} />
+        </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       <ul>
-        {persons.map(names => <li>{names.name}</li>)}
+        {persons.map(names => <li>{`${names.name} `}{names.number}</li>)}
       </ul>
     </div>
   )
