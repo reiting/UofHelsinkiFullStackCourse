0f11a220272a631d10f1f4b96146c535481b9a33
Rachel Eiting <reiting1@gmail.com>
2020-05-15 10:53:31 -0500

    finished part 2.9 creating search function and added a bit of styling to make it look better

--- Part 2/phonebook/src/App.js
+++ Part 2/phonebook/src/App.js
@@ -1,12 +1,24 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '205-364-8545' }
+    { name: 'Arto Hellas', number: '205-364-8545' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [searchTerm, setSearchTerm] = useState('')
+  const [searchResults, setSearchResults] = useState([])
+
+  useEffect(() => {
+    const results = persons.filter(person =>
+      person.name.toLowerCase().includes(searchTerm)
+    );
+    setSearchResults(results);
+  }, [searchTerm]);
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
@@ -15,6 +27,10 @@ const App = () => {
   const handleNumberChange = (event) => {
     setNewNumber(event.target.value)
   }
+
+  const handleSearchChange = event => {
+    setSearchTerm(event.target.value);
+  };
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -25,22 +41,28 @@ const App = () => {
     if (persons.some(cred => cred.name === newName)) {
       alert(`${newName} is already added to phonebook`);
     } else {
-    setPersons(persons.concat(personObject));
+      setPersons(persons.concat(personObject));
     }
   }
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with <input
+          value={searchTerm}
+          onChange={handleSearchChange} />
+      </div>
+      <h2>add a new</h2>
       <form onSubmit={addPerson}>
         <div>
           name: <input
             value={newName}
             onChange={handleNameChange} />
         </div>
-        <div>number: <input 
-            value={newNumber} 
-            onChange={handleNumberChange} />
+        <div>number: <input
+          value={newNumber}
+          onChange={handleNumberChange} />
         </div>
         <div>
           <button type="submit">add</button>
@@ -48,7 +70,9 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <ul>
-        {persons.map(names => <li>{`${names.name} `}{names.number}</li>)}
+        {searchResults.map(item => (
+          <li>{item.name}{item.number}</li>
+        ))}
       </ul>
     </div>
   )
