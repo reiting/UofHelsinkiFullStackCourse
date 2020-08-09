dd6c64ddf1ef497244bcfe1d4bd06239cdf662a1
Rachel Eiting <reiting1@gmail.com>
2020-08-09 13:51:46 -0500

    added frontend to the backend and made a few small changes to update backend to work with the frontend

--- Part 3/phonebook-backend/index.js
+++ Part 3/phonebook-backend/index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
+const cors = require('cors')
+
+app.use(cors())
 
 // Configure morgan to log body of POST request
 morgan.token('person', (req) => {
