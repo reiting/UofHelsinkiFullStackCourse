f690acbf357ef6f3c76b0f42ff91c2291d3d6171
Rachel Eiting <reiting1@gmail.com>
2020-08-30 19:51:42 -0500

    Finished 3.15-3.18 adding delete and put requests

--- Part 3/phonebook-backend/index.js
+++ Part 3/phonebook-backend/index.js
@@ -6,7 +6,6 @@ const morgan = require('morgan')
 const cors = require('cors')
 const Contact = require('./models/contact')
 
-
 app.use(cors())
 
 // Configure morgan to log body of POST request
@@ -23,34 +22,6 @@ app.use(
         ':method :url :status :res[content-length] - :response-time ms :person',
     ),
 )
-
-app.get('/api/persons', (request, response) => {
-    Contact.find({}).then(contacts => {
-        response.json(contacts)
-    })
-})
-
-// app.get('/api/persons/:id', (request, response) => {
-//     const id = Number(request.params.id)
-//     const person = persons.find(person => person.id === id)
-
-//     if (person) {
-//         response.json(person)
-//     }
-//     response.status(404).end()
-// })
-
-// app.get('/info', (request, response) => {
-//     const time = new Date()
-//     response.send(`<p>Phonebook has info for ${persons.length} people.</p> ${time}`)
-// })
-
-// app.delete('/api/persons/:id', (request, response) => {
-//     const id = Number(request.params.id)
-//     person = persons.filter(person => person.id !== id)
-
-//     response.status(204).end()
-// })
 
 app.post('/api/persons', (request, response) => {
     const body = request.body
@@ -76,18 +47,72 @@ app.post('/api/persons', (request, response) => {
     })
   })
 
-//     newPerson = persons.filter(person => person.name === person)
+  const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+  
+    if (error.name === 'CastError') {
+      return response.status(400).send({ error: 'malformatted id' })
+    } 
+  
+    next(error)
+  }
+  
+  app.use(errorHandler)
 
-//     if (newPerson) {
-//         return response.status(400).json({
-//             error: 'Name must be unique'
-//         })
-//     }
 
-//     persons = persons.concat(persons)
+app.get('/api/persons', (request, response) => {
+    Contact.find({}).then(contacts => {
+        response.json(contacts)
+    })
+})
 
-//     response.json(person)
-// })
+app.get('/api/persons/:id', (request, response) => {
+    Contact.findById(request.params.id)
+      .then(contact => {
+        if (contact) {
+          response.json(contact)
+        } else {
+          response.status(404).end()
+        }
+      })
+      .catch(error => next(error))
+  })
+
+  app.put('/api/persons/:id', (request, response, next) => {
+    const body = request.body
+  
+    const person = {
+        name: body.name,
+        number: body.number
+    }
+  
+    Contact.findByIdAndUpdate(request.params.id, person, { new: true })
+      .then(updatedPerson => {
+        response.json(updatedPerson)
+      })
+      .catch(error => next(error))
+  })
+
+  app.get('/info',(request,response)=>{
+    var d = Date(Date.now()); 
+    d = d.toString();
+    Contact.find({}).then(result=>{
+        response.send(
+            `<p>Phonebook has info for ${result.length} people</p>
+            ${d}
+            `
+        )
+    })
+    .catch(error => next(error))
+})
+
+app.delete('/api/persons/:id', (request, response, next) => {
+    Contact.findByIdAndRemove(request.params.id)
+      .then(result => {
+        response.status(204).end()
+      })
+      .catch(error => next(error))
+  })
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
