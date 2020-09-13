56fa11c0dafb3a336e17cdd18615f6f381b9d7c4
Rachel Eiting <reiting1@gmail.com>
2020-09-13 15:40:19 -0500

    finished part 4.4, testing totalLikes function

--- Part 4/utils/list_helper.js
+++ Part 4/utils/list_helper.js
@@ -2,6 +2,12 @@ const dummy = (blogs) => {
    return 1
 }
 
+const totalLikes = blogs => {
+    return blogs.reduce((acc, blog) => {
+        return acc + blog.likes;
+      }, 0);
+}
+
 module.exports = {
-    dummy
+    dummy, totalLikes
 }
