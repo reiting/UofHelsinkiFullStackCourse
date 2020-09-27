c79d8b0fe28fb77c6fefab0a51dc6fd3380ac3b3
Rachel Eiting <reiting1@gmail.com>
2020-09-27 12:06:35 -0500

    finished parts 4.13 and 4.14, implementing delete and put functions

--- Part 4/controllers/blogs.js
+++ Part 4/controllers/blogs.js
@@ -20,4 +20,36 @@ blogRouter.post('/', async (request, response) => {
     response.json(savedBlog.toJSON())
 })
 
+blogRouter.get('/:id', async (request, response) => {
+  const blog = await Blog.findById(request.params.id)
+  if (blog) {
+    response.json(blog.toJSON())
+  } else {
+    response.status(404).end()
+  }
+})
+
+blogRouter.put('/:id', (request, response, next) => {
+  const body = request.body
+
+  const blog = {
+    name: 'Cats Rule',
+    author: 'Tigress',
+    url: 'tigress.com',
+    likes: 5
+  }
+
+  Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
+    .then(updatedBlog => {
+      response.json(updatedBlog.toJSON())
+    })
+    .catch(error => next(error))
+})
+
+blogRouter.delete('/:id', async (request, response) => {
+  await Blog.findByIdAndRemove(request.params.id)
+  response.status(204).end()
+})
+
+
 module.exports = blogRouter
--- Part 4/tests/blogs_api.test.js
+++ Part 4/tests/blogs_api.test.js
@@ -4,6 +4,7 @@ const app = require('../app')
 const api = supertest(app)
 const helper = require('./test_helper')
 const Blog = require('../models/blog')
+const blogRouter = require('../controllers/blogs')
 
 beforeEach(async () => {
   await Blog.deleteMany({})
@@ -13,79 +14,119 @@ beforeEach(async () => {
   await blogObject.save()
 })
 
-test('blogs are returned as json', async () => {
-  await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/);
-});
+describe('Getting posts',() => {
+  test('blogs are returned as json', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/);
+  });
+  
+  test('all blogs are returned', async () => {
+    const response = await api.get('/api/blogs')
+    expect(response.body).toHaveLength(helper.initialBlogs.length)
+  })
+  
+  test('blogs should contain id property (not _id)', async () => {
+    const response = await api.get('/api/blogs')
+    expect(response.body[0].id).toBeDefined()
+  })
 
-test('all blogs are returned', async () => {
-  const response = await api.get('/api/blogs')
-  expect(response.body).toHaveLength(helper.initialBlogs.length)
+  test('viewing one specific note', async () => {
+      const blogsAtStart = await helper.blogsInDb()
+
+      const blogToView = blogsAtStart[0]
+
+      const resultBlog = await api
+        .get(`/api/blogs/${blogToView.id}`)
+        .expect(200)
+        .expect('Content-Type', /application\/json/)
+
+      expect(resultBlog.body).toEqual(blogToView)
+  })
 })
 
-test('blogs should contain id property (not _id)', async () => {
-  const response = await api.get('/api/blogs')
-  expect(response.body[0].id).toBeDefined()
+describe('POST function', () => {
+  test('a valid blog can be added ', async () => {
+    const newBlog = {
+      name: 'Cats Rule',
+      author: 'Tigress',
+      url: 'tigress.com',
+      likes: 5
+    }
+  
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  
+    const blogsAtend = await helper.blogsInDb()
+    expect(blogsAtend.length).toBe(helper.initialBlogs.length + 1)
+  
+    const name = blogsAtend.map(b => b.name)
+    expect(name).toContain(
+      'Cats Rule'
+    )
+  })
 })
 
-test('a valid blog can be added ', async () => {
-  const newBlog = {
-    name: 'Cats Rule',
-    author: 'Tigress',
-    url: 'tigress.com',
-    likes: 5
-  }
-
-  await api
+describe('Blogs missing properties', () => {
+  test('if likes property is missing, value will default to 0', async () => {
+    const newBlog = {
+      name: 'Cats Rule',
+      author: 'Tigress',
+      url: 'tigress.com',
+    };
+  
+    await api
     .post('/api/blogs')
     .send(newBlog)
     .expect(200)
     .expect('Content-Type', /application\/json/)
-
-  const blogsAtend = await helper.blogsInDb()
-  expect(blogsAtend.length).toBe(helper.initialBlogs.length + 1)
-
-  const name = blogsAtend.map(b => b.name)
-  expect(name).toContain(
-    'Cats Rule'
-  )
+  
+  const blogsAtEnd = await helper.blogsInDb()
+  
+  expect(blogsAtEnd[helper.initialBlogs.length].likes).toBe(0)
+  });
+  
+  test('if name and url properties are missing, receive a 400 status code', async () => {
+    const newBlog = {
+      author: 'Tigress',
+      likes: 5
+    };
+  
+    await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(400)
+    .expect('Content-Type', /application\/json/)
+  
+  const blogsAtEnd = await helper.blogsInDb()
+  expect(blogsAtEnd.length).toBe(helper.initialBlogs.length)
+  });
 })
 
-test('if likes property is missing, value will default to 0', async () => {
-  const newBlog = {
-    name: 'Cats Rule',
-    author: 'Tigress',
-    url: 'tigress.com',
-  };
-
-  await api
-  .post('/api/blogs')
-  .send(newBlog)
-  .expect(200)
-  .expect('Content-Type', /application\/json/)
-
-const blogsAtEnd = await helper.blogsInDb()
-
-expect(blogsAtEnd[helper.initialBlogs.length].likes).toBe(0)
-});
-
-test('if title and url properties are missing, receive a 400 status code', async () => {
-  const newBlog = {
-    author: 'Tigress',
-    likes: 5
-  };
-
-  await api
-  .post('/api/blogs')
-  .send(newBlog)
-  .expect(400)
-  .expect('Content-Type', /application\/json/)
-
-const blogsAtEnd = await helper.blogsInDb()
-expect(blogsAtEnd.length).toBe(helper.initialBlogs.length)
-});
+describe('Delete function', () => {
+  test('delete succeeds with status code 204 if id is valid', async () => {
+    const blogsAtStart = await helper.blogsInDb()
+    const blogToDelete = blogsAtStart[0]
+  
+    await api
+      .delete(`/api/blogs/${blogToDelete.id}`)
+      .expect(204)
+  
+    const blogsAtEnd = await helper.blogsInDb()
+  
+    expect(blogsAtEnd.length).toBe(
+      helper.initialBlogs.length - 1
+    )
+  
+    const name = blogsAtEnd.map(b => b.name)
+  
+    expect(name).not.toContain(blogToDelete.name)
+  })  
+})
 
 afterAll(() => {
   mongoose.connection.close()
