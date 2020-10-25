7c20aec45ef031644e3e9ee0046fd1e2e8409d7c
Rachel Eiting <reiting1@gmail.com>
2020-10-25 12:09:57 -0500

    finished part 5.7, adding ability to show or hide details of blogs

--- Part 5/bloglist-frontend/src/components/Blog.js
+++ Part 5/bloglist-frontend/src/components/Blog.js
@@ -1,9 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+const Blog = ({ blog }) => {
+  const [detailsVisible, setDetailsVisible] = useState(false)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5,
+  }
+
+  const hideWhenVisible = { display: detailsVisible ? 'none' : '' }
+  const showWhenVisible = { display: detailsVisible ? '' : 'none' }
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title}
+      </div>
+      <div style={hideWhenVisible}>
+        <button onClick={() => setDetailsVisible(true)}>View</button>
+      </div>
+      <div style={showWhenVisible}>
+        <li>
+          {blog.author}
+        </li>
+        <li>
+          {blog.url}
+        </li>
+        <li>
+          {blog.likes}
+          <button>Like</button>
+        </li>
+        <button onClick={() => setDetailsVisible(false)}>Hide</button>
+      </div>
+    </div>
+  )
+}
 
 export default Blog
+
+
