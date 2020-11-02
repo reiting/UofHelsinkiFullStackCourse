6c2bf079ad1fe0b1491df7232e45a7faf9afed93
Rachel Eiting <reiting1@gmail.com>
2020-11-02 08:56:36 -0600

    finished part 6.9, redux devtools and extracting store

---
+++ Part 6/redux-anecdotes/store.js
@@ -0,0 +1,17 @@
+import { createStore, combineReducers } from "redux";
+import { composeWithDevTools } from 'redux-devtools-extension'
+
+import anecdoteReducer from './reducers/anecdoteReducer'
+
+const reducer = combineReducers({
+    anecdotes: anecdoteReducer
+})
+
+const store = createStore(
+    reducer,
+    composeWithDevTools()
+  )
+  
+export default store
+
+    
