903111e0d5da9fff97fd94d5c772a24421a42d75
Rachel Eiting <reiting1@gmail.com>
2020-11-01 18:47:58 -0600

    finished part 6.7, separating out AnecdoteForm

--- Part 6/redux-anecdotes/src/App.js
+++ Part 6/redux-anecdotes/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-import { newAnecdote, voteForAnecdote } from './reducers/anecdoteReducer'
+import AnecdoteForm from './components/AnecdoteForm'
+import { voteForAnecdote } from './reducers/anecdoteReducer'
 
 const App = () => {
   const anecdotes = useSelector(state => state)
@@ -8,13 +9,6 @@ const App = () => {
 
   const vote = id => {
     dispatch(voteForAnecdote(id))
-  }
-
-  const addAnecdote = event => {
-    event.preventDefault()
-    const content = event.target.anecdote.value
-    event.target.anecdote.value = ''
-    dispatch(newAnecdote(content))
   }
 
   anecdotes.sort((a, b) => b.votes - a.votes)
@@ -33,13 +27,7 @@ const App = () => {
           </div>
         </div>
       )}
-      <h2>create new</h2>
-      <form onSubmit={addAnecdote}>
-        <div>
-          <input name='anecdote' />
-        </div>
-        <button type='submit'>create</button>
-      </form>
+      <AnecdoteForm />
     </div>
   )
 }
