c3692663a5ba051e2fe1cbd5d0acefbb26c8fa72
Rachel Eiting <reiting1@gmail.com>
2020-11-19 18:01:09 -0600

    finished 7.1, adding routing for / and /create

--- Part 7/routed-anecdotes/src/App.js
+++ Part 7/routed-anecdotes/src/App.js
@@ -1,17 +1,21 @@
 import React, { useState } from 'react'
+import {
+  BrowserRouter as Router,
+  Switch, Route, Link
+} from 'react-router-dom'
 
-const Menu = () => {
-  const padding = {
-    paddingRight: 5
-  }
-  return (
-    <div>
-      <a href='#' style={padding}>anecdotes</a>
-      <a href='#' style={padding}>create new</a>
-      <a href='#' style={padding}>about</a>
-    </div>
-  )
-}
+// const Menu = () => {
+//   const padding = {
+//     paddingRight: 5
+//   }
+//   return (
+//     <div>
+//       <a href='#' style={padding}>anecdotes</a>
+//       <a href='#' style={padding}>create new</a>
+//       <a href='#' style={padding}>about</a>
+//     </div>
+//   )
+// }
 
 const AnecdoteList = ({ anecdotes }) => (
   <div>
@@ -28,8 +32,8 @@ const About = () => (
     <p>According to Wikipedia:</p>
 
     <em>An anecdote is a brief, revealing account of an individual person or an incident.
-      Occasionally humorous, anecdotes differ from jokes because their primary purpose is not simply to provoke laughter but to reveal a truth more general than the brief tale itself,
-      such as to characterize a person by delineating a specific quirk or trait, to communicate an abstract idea about a person, place, or thing through the concrete details of a short narrative.
+    Occasionally humorous, anecdotes differ from jokes because their primary purpose is not simply to provoke laughter but to reveal a truth more general than the brief tale itself,
+    such as to characterize a person by delineating a specific quirk or trait, to communicate an abstract idea about a person, place, or thing through the concrete details of a short narrative.
       An anecdote is "a story with a point."</em>
 
     <p>Software engineering is full of excellent anecdotes, at this app you can find the best and add more.</p>
@@ -74,7 +78,7 @@ const CreateNew = (props) => {
         </div>
         <div>
           url for more info
-          <input name='info' value={info} onChange={(e)=> setInfo(e.target.value)} />
+          <input name='info' value={info} onChange={(e) => setInfo(e.target.value)} />
         </div>
         <button>create</button>
       </form>
@@ -122,15 +126,32 @@ const App = () => {
     setAnecdotes(anecdotes.map(a => a.id === id ? voted : a))
   }
 
+  const padding = {
+        paddingRight: 5
+      }
+
   return (
-    <div>
-      <h1>Software anecdotes</h1>
-      <Menu />
-      <AnecdoteList anecdotes={anecdotes} />
-      <About />
-      <CreateNew addNew={addNew} />
-      <Footer />
-    </div>
+    <Router>
+      <div>
+      <div>
+          <Link style={padding} to='/'>home</Link>
+          <Link style={padding}to='/create'>create new</Link>
+        </div>
+        <h1>Software anecdotes</h1>
+        <About />
+        <Switch>
+          <Route path='/create'>
+            <CreateNew addNew={addNew} />
+          </Route>
+          <Route path='/'>
+            <AnecdoteList anecdotes={anecdotes} />
+          </Route>
+        </Switch>
+        <div>
+          <Footer />
+        </div>
+      </div>
+    </Router>
   )
 }
 
