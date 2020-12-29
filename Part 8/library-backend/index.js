01927abbd25a450cd2063e4a05e6565c5edfde47
Rachel Eiting <reiting1@gmail.com>
2020-12-29 17:23:35 -0600

    finished 8.7, editAuthor mutation

--- Part 8/library-backend/index.js
+++ Part 8/library-backend/index.js
@@ -111,6 +111,10 @@ const typeDefs = gql`
       published: Int!
       genres: [String!]
     ): Book
+    editAuthor (
+      name: String!
+      setBornTo: Int!
+    ): Author
   }
 `
 
@@ -155,6 +159,14 @@ const resolvers = {
 
       books = books.concat(book)
       return book
+    },
+    editAuthor: (root, args) => {
+      const author = authors.find((a) => a.name === args.name);
+      if (!author) return null;
+
+      const authorToUpdate = { ...author, born: args.setBornTo };
+      authors = authors.map((a) => (a.name === args.name ? authorToUpdate : a));
+      return authorToUpdate;
     }
   }
 }
