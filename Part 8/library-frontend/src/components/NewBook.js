9b150c935202e7ea2dded547a9dd9421ef57b2cc
Rachel Eiting <reiting1@gmail.com>
2021-01-01 14:23:17 -0600

    finished part 8.10, adding a new book

--- Part 8/library-frontend/src/components/NewBook.js
+++ Part 8/library-frontend/src/components/NewBook.js
@@ -1,11 +1,17 @@
+import { useMutation } from '@apollo/client'
 import React, { useState } from 'react'
+import { ALL_BOOKS, CREATE_BOOK } from '../queries'
 
 const NewBook = (props) => {
   const [title, setTitle] = useState('')
-  const [author, setAuhtor] = useState('')
-  const [published, setPublished] = useState('')
+  const [author, setAuthor] = useState('')
+  let [published, setPublished] = useState('')
   const [genre, setGenre] = useState('')
   const [genres, setGenres] = useState([])
+
+  const [ createBook ] = useMutation(CREATE_BOOK, {
+    refetchQueries: [ { query: ALL_BOOKS } ]
+  })
 
   if (!props.show) {
     return null
@@ -13,12 +19,12 @@ const NewBook = (props) => {
 
   const submit = async (event) => {
     event.preventDefault()
-    
-    console.log('add book...')
+    published = Number(published);
+    createBook({ variables: { title, author, published, genres } })
 
     setTitle('')
     setPublished('')
-    setAuhtor('')
+    setAuthor('')
     setGenres([])
     setGenre('')
   }
@@ -42,7 +48,7 @@ const NewBook = (props) => {
           author
           <input
             value={author}
-            onChange={({ target }) => setAuhtor(target.value)}
+            onChange={({ target }) => setAuthor(target.value)}
           />
         </div>
         <div>
--- Part 8/library-frontend/src/index.js
+++ Part 8/library-frontend/src/index.js
@@ -14,6 +14,6 @@ const client = new ApolloClient({
 
 ReactDOM.render(
   <ApolloProvider client={client}>
-    <App />,
+    <App />
   </ApolloProvider>,
   document.getElementById('root'))
