20738d057fed75927512a15583ded95953d4780e
Rachel Eiting <reiting1@gmail.com>
2021-01-07 20:55:02 -0600

    got databased working for books and authors

--- Part 8/library-frontend/src/queries.js
+++ Part 8/library-frontend/src/queries.js
@@ -15,7 +15,9 @@ export const ALL_BOOKS = gql`
   query getAllBooks($author: String) {
     allBooks(author: $author) {
       title
-      author
+      author {
+        name
+      }
       published
       id
     }
@@ -36,7 +38,9 @@ export const CREATE_BOOK = gql`
       genres: $genres
     ) {
       title
-      author
+      author {
+        name
+      }
       published
       genres
       id
