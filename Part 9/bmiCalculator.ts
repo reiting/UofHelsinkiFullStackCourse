74dd5c3ce2949ab20c13077d0746a40542bf5bfb
Rachel Eiting <reiting1@gmail.com>
2021-03-13 20:41:22 -0600

    finished part 9.5, /bmi endpoint

--- Part 9/bmiCalculator.ts
+++ Part 9/bmiCalculator.ts
@@ -1,10 +1,12 @@
-const calculateBmi = (height: number, weight: number) => {
+export const calculateBmi = (height: number, weight: number):string => {
   const bmi = weight / (height / 100) ** 2;
 
   if (bmi < 18.5) return 'Underweight';
   if (bmi >= 18.5 && bmi < 25) return 'Normal weight';
   if (bmi > 25 && bmi <30) return 'Overweight';
   if (bmi >= 30) return 'Obese';
+
+  return 'Could not calculate BMI';
 }
 
 interface ParsedArguments {
--- Part 9/index.ts
+++ Part 9/index.ts
@@ -1,8 +1,23 @@
+import { calculateBmi } from './bmiCalculator';
 import express from 'express';
 const app = express();
 
 app.get('/hello', (_req, res) => {
   res.send('Hello Full Stack!');
+});
+
+app.get('/bmi', (req, res) => {
+  const { weight, height } = req.query;
+  const validParameters: boolean =
+    !isNaN(Number(height)) && !isNaN(Number(weight));
+  const bmi = calculateBmi(Number(height), Number(weight));
+
+  if (!validParameters || !weight || !height) {
+    res.send({
+      error: 'malformatted parameters',
+    });
+  }
+  res.send({ weight, height, bmi });
 });
 
 const PORT = 3003;
