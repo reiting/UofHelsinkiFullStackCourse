75238d6e0aa868c716c05e10a0b7baf51ebd829f
Rachel Eiting <reiting1@gmail.com>
2021-03-13 19:49:45 -0600

    added cmd line support

--- Part 9/bmiCalculator.ts
+++ Part 9/bmiCalculator.ts
@@ -1,10 +1,37 @@
 const calculateBmi = (height: number, weight: number) => {
   const bmi = weight / (height / 100) ** 2;
 
-  if (bmi < 18.5) return 'Underweight'
-  if (bmi >= 18.5 && bmi < 25) return 'Normal weight'
-  if (bmi >= 25 && bmi <30) return 'Overweight'
-  if (bmi >= 30) return 'Obese'
+  if (bmi < 18.5) return 'Underweight';
+  if (bmi >= 18.5 && bmi < 25) return 'Normal weight';
+  if (bmi >= 25 && bmi <30) return 'Overweight';
+  if (bmi >= 30) return 'Obese';
+}
+
+interface ParsedArguments {
+  height: number;
+  weight: number;
+}
+
+const parseArguments = (args: Array<string>): ParsedArguments => {
+  if (args.length < 4) throw new Error('Not enough arguments');
+  if (args.length > 4) throw new Error('Too many arguments');
+
+  if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+    return {
+      height: Number(args[2]),
+      weight: Number(args[3]),
+    };
+  } else {
+    throw new Error('Provided values were not numbers!');
+  }
+};
+
+try {
+  const { height, weight } = parseArguments(process.argv);
+  console.log(calculateBmi(height, weight));
+} catch (e) {
+  console.log('An error has occured: ', e.message);
+  console.log('USAGE: npm run calculateBmi height(cm) weight(kg)');
 }
 
 console.log(calculateBmi(180, 74))
