8c6edac5601e79cd12f788077aabc4b6ba4c1a95
Rachel Eiting <reiting1@gmail.com>
2021-03-13 20:02:05 -0600

    already did 9.3, but found a mistake in logic

--- Part 9/bmiCalculator.ts
+++ Part 9/bmiCalculator.ts
@@ -3,7 +3,7 @@ const calculateBmi = (height: number, weight: number) => {
 
   if (bmi < 18.5) return 'Underweight';
   if (bmi >= 18.5 && bmi < 25) return 'Normal weight';
-  if (bmi >= 25 && bmi <30) return 'Overweight';
+  if (bmi > 25 && bmi <30) return 'Overweight';
   if (bmi >= 30) return 'Obese';
 }
 
